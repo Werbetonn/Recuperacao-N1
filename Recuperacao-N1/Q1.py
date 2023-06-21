41a854648e77b4b2877464965303f8654d981c61
Werbetonn <115298840+Werbetonn@users.noreply.github.com>
2023-06-21 00:46:27 -0300

    Add files via upload

---
+++ Recuperacao-N1/Q1.py
@@ -0,0 +1,9 @@
+print('Digite seu cupom no campo abaixo e verifique se o mesmo é válido')
+
+cupom = int(input("Digite o número do cupom: "))
+
+if  75 <= cupom <= 208:
+    print('Cupom válido')
+else:
+    print('Cupom inválido')
+
