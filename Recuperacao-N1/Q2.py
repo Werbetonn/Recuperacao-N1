54f087505a0cba75849490e269bc0478dd38f37a
Werbetonn <115298840+Werbetonn@users.noreply.github.com>
2023-06-27 01:27:20 -0300

    Update Q2.py

--- Recuperacao-N1/Q2.py
+++ Recuperacao-N1/Q2.py
@@ -1,1 +1,11 @@
-print('Não conssegui resolver :(')
+rank = {}
+quantidade = int(input('Quantos livros deseja adicionar? '))
+for i in range(quantidade):
+
+    livro = input('Digite o nome de um livro: ')
+    posicao = input('Digite a posição desse livro: ')
+
+    rank[livro]=posicao
+
+for livro, posicao in sorted(rank.items()):
+    print(f'{posicao}- {livro}')
